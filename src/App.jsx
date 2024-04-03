6c1a9ee46d8f62ed92fd6b77efbbb3935d698264
Bruno Cordoba <brunonahuel87@gmail.com>
2024-04-03 20:43:34 -0300

    comentario principal

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import Carrito from "./components/Carrito"
+import DetalleProducto from "./components/DetalleProducto";
+function App() {
+  
+
+  return (
+    <>
+    <DetalleProducto/>
+    </>
+  )
+}
+
+export default App
